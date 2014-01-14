9a083e3571a09b4f0e3168eb6ec2cadddf574e73
charlesgael <charlesgael.djedai@orange.fr>
2014-01-14 20:07:16 +0100

    Reindenting MontecarloIa

--- src/java/org/gc/amino/ia/mc/MontecarloIa.java
+++ src/java/org/gc/amino/ia/mc/MontecarloIa.java
@@ -17,23 +17,23 @@ public class MontecarloIa implements IaDeliveryInterface {
 	public static final long INTERVAL = 1000;
 
 	private Mote me;
-	
+
 	private boolean acceptFrame;
 	private long nextFrame;
 	private SearchNode mc;
-	
+
 	private Action lastAction;
 	private int nbActions;
 
-    public static void main( String[] argv ) throws IOException {
-        IaLauncher.launch( argv, new MontecarloIa() );
-    }
+	public static void main( String[] argv ) throws IOException {
+		IaLauncher.launch( argv, new MontecarloIa() );
+	}
 
 	@Override
 	public void init(PointD size) {
 		Game.init(new TerrainMap(size));
-        acceptFrame = true;
-        //actions = new LinkedList<>();
+		acceptFrame = true;
+		//actions = new LinkedList<>();
 	}
 
 	@Override
@@ -51,30 +51,30 @@ public class MontecarloIa implements IaDeliveryInterface {
 			System.out.println("Next computation");
 			acceptFrame = false;
 			nextFrame = Calendar.getInstance().getTime().getTime() + INTERVAL;
-			
+
 			Board b = new Board(you, otherMotes);
 			mc = new SearchNode(null, b, null);
 			mc.startComputing();
-			
+
 			return action;
 		}
 		else if(Calendar.getInstance().getTime().getTime() > nextFrame) {
 			acceptFrame = true;
 		}*/
-		
+
 		double speed = Math.sqrt( Math.pow(you.getSpeed().x, 2) + Math.pow(you.getSpeed().y, 2));
-		
+
 		if (mc == null) {
 			System.out.println("Next computation");
-			
+
 			Iterator<Mote> it = otherMotes.iterator();
-			
+
 			while(it.hasNext()){
 				Mote m = it.next();
 				if (m.getRadius() < you.getRadius()*0.05)
 					it.remove();
 			}
-			
+
 			Board b = new Board(you, otherMotes);
 			mc = new SearchNode(null, b, null);
 			mc.start();
@@ -82,9 +82,9 @@ public class MontecarloIa implements IaDeliveryInterface {
 		else if (mc.isResultAvailable()) {
 			Action a = mc.getBestChild().getAction();
 			System.out.println("Result available "+a);
-			
+
 			mc = null;
-			
+
 			if (!isBigEnough(you, otherMotes)) {
 				System.out.println("Not big enough : PLAY");
 				if (lastAction == null || !lastAction.isOpposite(a))
@@ -101,7 +101,7 @@ public class MontecarloIa implements IaDeliveryInterface {
 		else if (speed < 0.2 && !goodAngle(you.getSpeed(), you)) {
 			return lastAction.fastPoint(you);
 		}
-		
+
 		return null;
 	}
 
@@ -109,9 +109,9 @@ public class MontecarloIa implements IaDeliveryInterface {
 		if (lastAction == null) return true;
 		PointD will = lastAction.fastPoint(you);
 		if (will == null) return true;
-		
+
 		double angle = 0;
-		
+
 		if (speed.x > 0) {
 			if (speed.y >= 0) {
 				angle = Math.atan(speed.y / speed.x);
@@ -131,9 +131,9 @@ public class MontecarloIa implements IaDeliveryInterface {
 				angle = 3*Math.PI /2;
 			}
 		}
-		
+
 		double angle2 = 0;
-		
+
 		if (will.x > 0) {
 			if (will.y >= 0) {
 				angle2 = Math.atan(will.y / will.x);
@@ -153,19 +153,19 @@ public class MontecarloIa implements IaDeliveryInterface {
 				angle2 = 3*Math.PI /2;
 			}
 		}
-		
+
 		System.out.println("Angle1 : "+angle+"\nAngle2 : "+angle2+"\nGood : "+(Math.abs(angle2 - angle) < Math.PI /2));
-		
+
 		return Math.abs(angle2 - angle) < Math.PI /2;
 	}
 
 	private boolean isBigEnough(Mote you, List<Mote> otherMotes) {
 		double maxRadius = 0;
-		
+
 		for (Mote m : otherMotes) {
 			maxRadius = Math.max(maxRadius, m.getRadius());
 		}
-		
+
 		return you.getRadius()*0.9 > maxRadius;
 	}
 }
